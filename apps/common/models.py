3f57df02a272c42f402edaee48b8db4370189013
Imyaminov <250503iaa@gmail.com>
2023-06-13 10:40:39 +0500

    fix: phone number RegexValidater expression in User model changed

--- apps/common/models.py
+++ apps/common/models.py
@@ -7,8 +7,8 @@ from helpers.models import TimeStampedModel
 
 class User(AbstractUser, TimeStampedModel):
     _validate_phone = RegexValidator(
-        regex=r"^9\d{12}$",
-        message='Phone number must start from 9 and contain 12 characters. For example: 998998065999'
+        regex=r"^998\d{9}$",
+        message='Phone number must start from 998 and contain 9 characters. For example: 998998065999'
     )
 
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
