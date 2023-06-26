c28072862deb6cefcaa5fb0a709c465ac77e580c
Imyaminov <250503iaa@gmail.com>
2023-06-26 09:28:26 +0500

    fix: pagination's page_size_query set

--- apps/room/paginations.py
+++ apps/room/paginations.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 
 class RoomListPageNumberPagination(PageNumberPagination):
     page_size = 10
-    page_query_param = 'page_size'
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
 
     def get_paginated_response(self, data):
         return Response({
